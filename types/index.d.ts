12082e1ad3dd80d548d7e3e4a1d44b39e3c50c5f
T-Ev <tng22@wildcats.unh.edu>
2020-01-14 13:34:34 -0500

    Add files via upload

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,14 +2,23 @@
 // Licensed under the MIT License.
 
 declare namespace AppCenter {
-    export function getInstallId(
-        success: (installId: string) => void,
-        error: (error: any) => void
-    ): void;
+    let Analytics: {
+        trackEvent(
+            eventName: string,
+            properties: {},
+            success: () => void,
+            error: (error: any) => void
+        ): void;
 
-    export function setUserId(
-        userId: string,
-        success: () => void,
-        error: (error: any) => void
-    ): void;
+        isEnabled(
+            success: (isEnabled: boolean) => void,
+            error: (error: any) => void
+        ): void;
+
+        setEnabled(
+            enabled: boolean,
+            success: () => void,
+            error: (error: any) => void
+        ): void;
+    };
 }
